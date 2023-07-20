25a5a5dc008e0c7cd70b60228ac7f48474c35166
KIMGEUNDU <vlkoklv@naver.com>
2023-07-21 02:08:49 +0900

    전체 틀 완성

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -1,4 +1,3 @@
-
 /* 
 
 1. 클릭 이벤트 활성화
@@ -9,13 +8,62 @@
 
 */
 
+const nav = getNode(".nav");
+const li = getNodes("li");
+const nickName = getNode(".nickName");
+const img = getNode(".visual img");
+const body = getNode("body");
+const audio = document.createElement("audio");
 
+//오디오 추가 함수
+function addAudio(name) {
+  const audioSrc = `/mission02/client/assets/audio/${name.toLowerCase()}.m4a`;
+  const audioPlay = new AudioPlayer(audioSrc);
+  audioPlay.play();
+}
 
+//배경화면 변경 함수
+function setBgColor(color1, color2 = "#000") {
+  setCss(
+    body,
+    "background",
+    `linear-gradient(to bottom, ${color1}, ${color2})`
+  );
+}
 
+//이미지 변경 함수
+function setImage(src, alt) {
+  attr(img, "src", `./assets/${src}.jpeg`);
+  attr(img, "alt", `${alt}`);
+}
 
+// 이름 변경 함수
+function setNameText(name) {
+  nickName.textContent = name;
+}
+// 클래스 추가 함수
+function addActive(removeTarget, addTarget, className) {
+  removeTarget.forEach((i) => {
+    removeClass(i, className);
+  });
 
+  addClass(addTarget, className);
+}
 
+//클릭이벤트
+function handleClick(e) {
+  const target = e.target.closest("li");
+  if (!target) {
+    return;
+  }
+  const index = target.dataset.index;
+  const dataIndex = data[index - 1];
 
+  addActive(li, target, "is-active");
+  setNameText(dataIndex.name);
+  setImage(dataIndex.name, dataIndex.alt);
+  setBgColor(dataIndex.color[0], dataIndex.color[1]);
+  addAudio(dataIndex.name);
+}
 
-
-
+nav.addEventListener("click", handleClick);
